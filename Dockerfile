d1d41c839f7d6bcd9e0f4755528e9973058a3fee
d.shishlov <dima.shishlov89@mail.ru>
2023-06-16 01:10:18 +0300

    ПОтылватп

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM ubuntu:20.04
 
 COPY entrypoint.sh /entrypoint.sh
+COPY cred-helper.sh /cred-helper.sh
 
 RUN apt update
 RUN apt install -y git
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ git checkout "${GITHUB_REF##*/}"
 
 branch="$(git symbolic-ref --short HEAD)"
 
-sh -c "git config --global --add safe.directory '*'"
+sh -c "git config --global --add safe.directory /github/workspace"
 sh -c "git config --global credential.username $GITLAB_USERNAME"
-sh -c "git config --global core.askPass $GITLAB_PASSWORD"
+sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
 sh -c "git remote add mirror $*"
 sh -c "git pull"
